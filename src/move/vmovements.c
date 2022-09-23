40ac305527fd3e6edf6fc8225536ddca48253a05
mtellal <mez.tellal@gmail.com>
2022-09-23 10:54:30 +0200

    update vmovements

--- src/move/vmovements.c
+++ src/move/vmovements.c
@@ -12,69 +12,60 @@
 
 #include "cub3d.h"
 
-void    moveVRIGHT(t_frame *img)
-{    
-    double  angle = 0.003;
+double      rotateX(double angle, double x, double y, int r)
+{
+    if (r)
+        return (x * -cos(angle) + y * sin(angle));
+    return (x * cos(angle) + y * -sin(angle));
+}
 
-    double	apos_x = img->triangle.a.x - img->triangle.milieu.x;
-    double	apos_y = img->triangle.milieu.y - img->triangle.a.y;
+double      rotateY(double angle, double x, double y, int r)
+{
+    if (r)
+        return (x * -sin(angle) + y * -cos(angle));
+    return ((x * sin(angle) + y * cos(angle)));
+}
 
-    img->triangle.a.x = (apos_x * cos(angle)) + (apos_y * -sin(angle));
-    img->triangle.a.x += img->triangle.milieu.x;
+void        rotatePoint(double angle, double *x, double *y, t_coor origine)
+{
+    double opos_x = *x - origine.x;
+    double opos_y = origine.y - *y;
 
-    img->triangle.a.y = (apos_x * sin(angle)) + (apos_y * cos(angle));
-    img->triangle.a.y = img->triangle.milieu.y - img->triangle.a.y;
- 
-    double	bpos_x = img->triangle.b.x - img->triangle.milieu.x;
-    double	bpos_y = img->triangle.milieu.y - img->triangle.b.y;
+    *x = rotateX(angle, opos_x, opos_y, 0);
+    *x += origine.x;
+    *y = rotateY(angle, opos_x, opos_y, 0);
+    *y = origine.y - *y;
+}
 
-    img->triangle.b.x = (bpos_x * cos(angle)) + (bpos_y * -sin(angle));
-    img->triangle.b.x += img->triangle.milieu.x;
+void        rrotatePoint(double angle, double *x, double *y, t_coor origine)
+{
+    double opos_x = origine.x - *x;
+    double opos_y = origine.y - *y;
 
-    img->triangle.b.y = (bpos_x * sin(angle)) + (bpos_y * cos(angle));
-    img->triangle.b.y = img->triangle.milieu.y - img->triangle.b.y;
-
-
-    double	cpos_x = img->triangle.c.x - img->triangle.milieu.x;
-    double	cpos_y = img->triangle.milieu.y - img->triangle.c.y;
-
-    img->triangle.c.x = (cpos_x * cos(angle)) + (cpos_y * -sin(angle));
-    img->triangle.c.x += img->triangle.milieu.x;
-
-    img->triangle.c.y = (cpos_x * sin(angle)) + (cpos_y* cos(angle));
-    img->triangle.c.y = img->triangle.milieu.y - img->triangle.c.y;
+    *x = rotateX(angle, opos_x, opos_y, 1);
+    *x += origine.x;
+    *y = rotateY(angle, opos_x, opos_y, 1);
+    *y += origine.y;
 }
 
 void    moveVLEFT(t_frame *img)
+{    
+    double  angle = 0.003;
+
+    rotatePoint(angle, &img->triangle.a.x, &img->triangle.a.y, img->triangle.milieu);
+
+    rotatePoint(angle, &img->triangle.b.x, &img->triangle.b.y, img->triangle.milieu);
+
+    rotatePoint(angle, &img->triangle.c.x, &img->triangle.c.y, img->triangle.milieu);
+}
+
+void    moveVRIGHT(t_frame *img)
 {
     double		angle = 0.003;
 
-    double	apos_x = img->triangle.milieu.x - img->triangle.a.x;
-    double	apos_y = img->triangle.milieu.y - img->triangle.a.y;
+    rrotatePoint(angle, &img->triangle.a.x, &img->triangle.a.y, img->triangle.milieu);
 
-    img->triangle.a.x = (apos_x * -cos(angle)) + (apos_y * sin(angle));
-    img->triangle.a.x += img->triangle.milieu.x;
+    rrotatePoint(angle, &img->triangle.b.x, &img->triangle.b.y, img->triangle.milieu);
 
-    img->triangle.a.y = (apos_x * -sin(angle)) + (apos_y* -cos(angle));
-    img->triangle.a.y += img->triangle.milieu.y;
-
-
-    double	bpos_x = img->triangle.milieu.x - img->triangle.b.x;
-    double	bpos_y = img->triangle.milieu.y - img->triangle.b.y;
-
-    img->triangle.b.x = (bpos_x * -cos(angle)) + (bpos_y * sin(angle));
-    img->triangle.b.x += img->triangle.milieu.x;
-
-    img->triangle.b.y = (bpos_x * -sin(angle)) + (bpos_y * -cos(angle));
-    img->triangle.b.y += img->triangle.milieu.y;
-
-
-    double	cpos_x = img->triangle.milieu.x - img->triangle.c.x;
-    double	cpos_y = img->triangle.milieu.y - img->triangle.c.y;
-
-    img->triangle.c.x = (cpos_x * -cos(angle)) + (cpos_y * sin(angle));
-    img->triangle.c.x += img->triangle.milieu.x;
-
-    img->triangle.c.y = (cpos_x * -sin(angle)) + (cpos_y* -cos(angle));
-    img->triangle.c.y += img->triangle.milieu.y;
+    rrotatePoint(angle, &img->triangle.c.x, &img->triangle.c.y, img->triangle.milieu);
 }
